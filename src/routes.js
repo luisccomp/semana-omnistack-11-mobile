ce4d67500736f9d8a59f52fc81cd2df29608ebbd
Luis Carlos Silva Junior <luisccomp@gmail.com>
2020-03-27 17:53:54 -0300

    First project commit

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Details from './pages/Details';
+import Incidents from './pages/Incidents';
+
+
+const AppStack = createStackNavigator();
+
+function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{headerShown: false}}>
+                <AppStack.Screen name="Incidents" component={Incidents} />
+                <AppStack.Screen name="Details" component={Details} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default Routes;
